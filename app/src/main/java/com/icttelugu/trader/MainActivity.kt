beb659108dc162afe7031b17532a472ed6c7c7d0
kattakiran789-design <kattakiran789@gmail.com>
2026-08-06 20:32:12 +0530

    Change welcome message and modify TextView properties
    Updated welcome message and adjusted text size and padding.

--- app/src/main/java/com/icttelugu/trader/MainActivity.kt
+++ app/src/main/java/com/icttelugu/trader/MainActivity.kt
@@ -8,13 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // స్క్రీన్ పై టెక్స్ట్ చూపించడానికి చిన్న లేఅవుట్
         val textView = TextView(this)
-        textView.text = "Welcome to ICT Telugu Trader!"
-        textView.textSize = 20f
-        textView.setPadding(50, 100, 50, 50)
-        
+        textView.text = "App Started Successfully!"
+        textView.textSize = 22f
         setContentView(textView)
     }
 }
-
